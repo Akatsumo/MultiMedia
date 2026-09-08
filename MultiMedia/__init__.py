03b245c08ecf9391599c7dbac22f480f51dbf6c4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-09-08 12:44:37 +0530

    Add files via upload

--- MultiMedia/__init__.py
+++ MultiMedia/__init__.py
@@ -1,1 +1,17 @@
+import config
+from pyrogram import Client
 
+# --------------------------------- Client --------------------------------- #
+
+app = Client(
+    ":MultiMedia:",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+)
+
+# --------------------------------- Global Variables --------------------------------- #
+
+BOT_ID = None
+BOT_NAME = None
+BOT_USERNAME = None
---
+++ MultiMedia/__main__.py
@@ -0,0 +1,29 @@
+import asyncio
+import importlib, MultiMedia
+from pyrogram import idle 
+from MultiMedia.modules import ALL_MODULES
+
+loop = asyncio.get_event_loop()
+
+async def init_bot():
+    await MultiMedia.app.start()
+    print("[√] Bot Started")
+    
+    getme = await MultiMedia.app.get_me()
+    MultiMedia.BOT_ID = getme.id
+    MultiMedia.BOT_USERNAME = getme.username
+    MultiMedia.BOT_NAME = f"{getme.first_name} {getme.last_name}" if getme.last_name else getme.first_name
+    print(f"»» Booted as {MultiMedia.BOT_NAME} (@{MultiMedia.BOT_USERNAME})")
+  
+# ------------------------------- LOAD MODULES ------------------------------- #
+    for all_module in ALL_MODULES:
+        importlib.import_module("MultiMedia.modules." + all_module)
+
+    print("[√] Bot Deploy Successful ✨")
+    await idle()
+    await MultiMedia.app.stop()
+    print("Bot Stopped!!")
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init_bot())
